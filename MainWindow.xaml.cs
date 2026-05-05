4850944c9507d104353c16d455ba35687134ccca
Rin-ro <korikovadara4@gmail.com>
2026-05-05 22:58:47 +0600

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apteka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new UsersPage());
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            this.Title = ((Page)e.Content).Title;
+        }
+
+        private void UsersBut_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new UsersPage());
+        }
+
+        private void MedicinesBut_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new MedicinesPage());
+        }
+
+        private void CategoriesBut_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new CategoriesPage());
+        }
+
+        private void OrdersBut_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new OrdersPage());
+        }
+
+        private void OrdersPositionsBut_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new OrdersPositionsPage());
+        }
+    }
+}
---
+++ UsersPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apteka
+{
+    /// <summary>
+    /// Логика взаимодействия для UsersPage.xaml
+    /// </summary>
+    public partial class UsersPage : Page
+    {
+        //private string connectionString = @"Server=computer\sqlexpress;Database=Apteka;Trusted_Connection=True;";
+        private string connectionString = @"Server=localhost;Database=Apteka;Trusted_Connection=True;";
+        public UsersPage()
+        {
+            InitializeComponent();
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "select * from customers";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(dataTable);
+            }
+            usersDataTable.ItemsSource = dataTable.DefaultView;
+        }
+    }
+}
